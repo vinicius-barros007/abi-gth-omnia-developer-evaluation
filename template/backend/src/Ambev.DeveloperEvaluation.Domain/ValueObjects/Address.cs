3b2510f50043bdfe2641a3cb3a1da007ca0f5a12
Vinícius Camargo Barros <vinicius_barros007@hotmail.com>
2025-03-22 11:50:43 -0300

    Initial commit

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/ValueObjects/Address.cs
@@ -0,0 +1,45 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.ValueObjects;
+
+public class Address : ValueObject
+{
+    public Address(
+        string street,
+        int number, 
+        string city, 
+        string state, 
+        string country, 
+        string zipCode
+    )
+    {
+        Street = street;
+        Number = number;
+        City = city;
+        State = state;
+        Country = country;
+        ZipCode = zipCode;
+    }
+
+    public string City { get; private set; }
+
+    public string Street { get; private set; }
+
+    public int Number { get; private set; }
+
+    public string ZipCode { get; private set; }
+        
+    public string State { get; private set; }
+
+    public string Country { get; private set; }
+    
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Street;
+        yield return Number;
+        yield return City;
+        yield return State;
+        yield return Country;
+        yield return ZipCode;
+    }
+}
